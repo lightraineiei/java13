c69a5dcb049f8ced62b46a0e5275b19d0dc17338
lightraineiei <91459035+lightraineiei@users.noreply.github.com>
2022-01-09 20:25:31 +0800

    Update and rename Class05.java to hw05.java

---
+++ hw05.java
@@ -0,0 +1,23 @@
+
+public class hw05 
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		try 
+		{
+			int num = 12; 
+			int den []= {12,0,3,0,0,4};
+			for (int i=0; i<10;i++)
+				System.out.println("ans="+num/den[i]);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("除數為0");
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("陣列索引值超出了範圍");
+		}
+	}
+}
