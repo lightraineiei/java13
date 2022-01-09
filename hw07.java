852b5920be12de68f5e134cf2f0869559048fe2a
lightraineiei <91459035+lightraineiei@users.noreply.github.com>
2022-01-09 21:07:34 +0800

    Add files via upload

---
+++ hw07.java
@@ -0,0 +1,23 @@
+
+public class hw07
+{
+	public static void test(int num,int den) throws ArithmeticException
+	{
+		System.out.println(num+"/"+den+"="+num/den);
+	}
+	public static void main(String[] args)
+	{
+		try
+		{
+			test(5,0);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("°£¼Æ¬° 0");
+		}
+		finally
+		{
+			System.out.println("end of main() method!!");
+		}
+	}
+}
