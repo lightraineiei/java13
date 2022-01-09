c0c2b792ca1bfa7e49c11e80edc039baaa9ded92
lightraineiei <91459035+lightraineiei@users.noreply.github.com>
2022-01-09 20:10:17 +0800

    Update and rename Class03.java to hw03.java

---
+++ hw03.java
@@ -0,0 +1,21 @@
+
+public class hw03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try 
+		{
+			int num = 12, den = 0;
+			int ans = num / den;
+			System.out.println("ans=" + ans);
+		} 
+		catch (ArithmeticException e) 
+		{
+			System.out.println("除數為 0");
+		} 
+		finally 
+		{
+			System.out.println("end of main() method!!");
+		}
+	}
+}
